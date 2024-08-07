0150a926ede83abd393df89e5e4b526af9b42900
PavelSpectr <titsubishi@yandex.ru>
2024-08-07 14:13:41 +0800

    Add adaptive stucture with base settings CSS. !!!Not optimized for Vue!!!

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script>
+
+</script>
+
+<template>
+  <header class="top-panel">
+    <section class="logo">
+      Logo
+    </section>
+    <section class="time">
+      Time
+    </section>
+    <section class="weather">
+      Weather
+    </section>
+  </header>
+
+  <main class="welcome">
+    <section class="photo">
+      Photo
+    </section>
+    <section class="greeting">
+      Greeting
+    </section>
+  </main>
+  <aside class="services">
+    <section class="car">
+      Car
+    </section>
+    <section class="family">
+      Family
+    </section>
+    <section class="house">
+      House
+    </section>
+    <section class="business">
+      Business
+    </section>
+  </aside>
+
+
+  <footer>
+    <address class="contacts">
+      <ul>
+        <li>г. Иркутск</li>
+        <li>664047 ул. Трилиссера, 87</li>
+        <li>Офис 306 (3й этаж)</li>
+        <li>Тел: <a href="tel:+79021718494">+7(902)171-84-94</a></li>
+        <li>E-mail: <a href="mailto:favor606@mail.ru">favor606@mail.ru</a> </li>
+      </ul>
+    </address>
+  </footer>
+
+  <!--  <nav class="menu">
+    <section class="call">Call</section>
+    <section class="message">Message</section>
+    <section class="location>">Location</section>
+  </nav>-->
+</template>
+
+<style scoped>
+
+</style>
